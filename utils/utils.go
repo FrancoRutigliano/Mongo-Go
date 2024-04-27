acdb11633849f0f50ca5d4b24fa0bd2b68858ee4
FrancoRutigliano <francorutigliano11@gmail.com>
2024-04-27 19:27:40 -0300

    adding function to utils WriteJson && WriteErr --> These functions are usefull

--- utils/utils.go
+++ utils/utils.go
@@ -15,3 +15,17 @@ func ParseJSON(r *http.Request, payload any) error {
 
 	return json.NewDecoder(r.Body).Decode(payload)
 }
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	// agregar a cabeceras content -> json
+	w.Header().Add("Content-Type", "application/json")
+	// add status
+	w.WriteHeader(status)
+
+	// encode data --> json
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteErr(w http.ResponseWriter, status int, err error) {
+	WriteJson(w, status, map[string]string{"error": err.Error()})
+}
